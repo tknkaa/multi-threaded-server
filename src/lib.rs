ae99070ed022242a290af00cfc96c32ff7148c69
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-04-09 20:07:20 +0900

    Workerの定義

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 use std::thread;
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
-        let mut threads = Vec::with_capacity(size);
-        for _ in 0..size {
-            //
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id));
         }
-        ThreadPool { threads }
+        ThreadPool { workers }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -19,5 +19,17 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static,
     {
         //
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+        Worker { id, thread }
     }
 }
